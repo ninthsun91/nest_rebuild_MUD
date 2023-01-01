553501ee5c19f638840b07969118988db63726e6
ninthsun91 <ninthsun91@gmail.com>
2023-01-02 03:28:26 +0900

    [front] page initializer / print handler

--- front/views/public/js/index.js
+++ front/views/public/js/index.js
@@ -1,37 +1,57 @@
 const SERVER_URL = `localhost:3000`;
 let socket = undefined;
 
+const commendLine = document.getElementById('commendLine');
+const commendForm = document.getElementById('commendForm');
+const commendInput = document.getElementById('commendInput');
+
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('index.js is ready');
 
     socket = io.connect(`ws://${SERVER_URL}/login`);
     openSocketListener(socket);
     openEventListener(socket);
+
+    frontPageInitializer(socket);
 });
 
+const frontPageInitializer = (socket) => {
+    localStorage.clear();
+    
+    const input = { line: 'load' }
+    socket.emit('none', input, ({ field, script }) => {
+        localStorage.setItem('field', field);
+        commendLine.innerHTML = script;
+    });
+}
 
-const commendLine = document.getElementById('commendLine');
-const commendForm = document.getElementById('commendForm');
-const commendInput = document.getElementById('commendInput');
+
 
 const openEventListener = (socket) => {
     commendForm.addEventListener('submit', (event) => commendSubmitHandler(event, socket));
 }
 
 const openSocketListener = (socket) => {
-    socket.on('message', (data) => {
-        console.log('received', data);
-    });
+    socket.on('print', printHandler);
+}
+
+const printHandler = ({ field, script, userInfo }) => {
+    localStorage.setItem('field', field);
+    localStorage.setItem('userInfo', userInfo);
+
+    commendLine.innerHTML = script;
 }
 
 
 const commendSubmitHandler = (event, socket) => {
     event.preventDefault();
 
-    const input = commendInput.value
-    console.log('submit', input);
+    const line = commendInput.value;
+    commendInput.value = '';
+    console.log(`submit: '${line}'`);
 
-    socket.emit('test', { input }, (data) => {
-        console.log('received', data);
-    });
+    const field = localStorage.getItem('field');
+    const userInfo = localStorage.getItem('userInfo');
+
+    socket.emit(field, { line, userInfo });
 }
